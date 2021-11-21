4521e6fa9a41f18a6956e281714923fbfe436469
Renzo <renzo607@gmail.com>
2021-11-20 23:16:58 -0500

    Initial Commit

---
+++ little-chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  little-chat
+//
+//  Created by Renzo Paul Chamorro on 20/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
