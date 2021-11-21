2c9a12084e8f4d81f0b897dc79944b20c110dd76
Renzo <renzo607@gmail.com>
2021-11-20 23:58:57 -0500

    create welcome screen with branding, colors

---
+++ little-chat/Model/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  little-chat
+//
+//  Created by Renzo Paul Chamorro on 20/11/21.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "🍎LittleChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
